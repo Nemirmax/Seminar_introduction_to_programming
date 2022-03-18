8b167ee31cb59ec8b747be6b3b357e5adeb3d7e9
Maxim <maxim.nemer@mail.ru>
2022-03-18 14:08:56 +0300

    добавили задачу 11

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -25,10 +25,19 @@
 
 //Задача 11: 
 
-Console.WriteLine("Ввести число в диапазоне от 10 до 99");
-int numberN = Convert.ToInt32(Console.ReadLine());
-if (numberN >= 10 & numberN <= 99) {
-    int FirstDigit = numberN % 10;
-    int SecondDigit = numberN /10;
+// Console.WriteLine("Ввести число в диапазоне от 10 до 99");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+// if (numberN >= 10 & numberN <= 99) {
+//     int FirstDigit = numberN % 10;
+//     int SecondDigit = numberN /10;
+//     if (FirstDigit >= SecondDigit){
+//         Console.WriteLine ("Числа равны");
+//     }else if (FirstDigit > SecondDigit){
+//         Console.WriteLine("Первое наибольшее");
+//     }else{
+//         Console.WriteLine("Второе наибольшее");
+//     }
 
-}
+// }
+
+//Задача 12
