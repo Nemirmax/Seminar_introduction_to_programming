d48562f58572e08e769a17b422b1a7db9ba77e46
Maxim <maxim.nemer@mail.ru>
2022-03-28 16:44:54 +0300

    добавил 2 задачи дз

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -81,3 +81,38 @@
 // {
 //     Console.WriteLine("Неверный номер дня!");
 // }
+
+// 10. Показать вторую цифру трёхзначного числа
+
+// Console.Clear();
+// System.Console.WriteLine();
+// int number = new Random().Next(100, 1000);
+
+// System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+
+// System.Console.WriteLine(); 
+
+// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+// Console.Clear();
+// System.Console.WriteLine();
+
+// int Number1 = 50;
+// int Number2 = 8;
+// int result = 0;
+
+// int MultipleNumber()
+// {
+//     result = Number1 % Number2;
+//     return result;
+// }
+
+// MultipleNumber();
+
+// System.Console.WriteLine();
+// if (result == 0) System.Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+// else
+// {
+//     Console.WriteLine($"Число {Number1} не кратно числу {Number2}, остаток от деления равен {Number1 % Number2}");
+// }
+// System.Console.WriteLine();
