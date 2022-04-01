f99baeac4231349f9fb95fb9bd0593311f2e088e
Maxim <maxim.nemer@mail.ru>
2022-04-01 14:17:53 +0300

    добавили задачу 23

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -116,3 +116,36 @@
 //     Console.WriteLine($"Число {Number1} не кратно числу {Number2}, остаток от деления равен {Number1 % Number2}");
 // }
 // System.Console.WriteLine();
+
+// 24. Найти кубы чисел от 1 до N
+
+// Console.Clear();
+// System.Console.WriteLine();
+
+// System.Console.Write("Введите число:   ");
+// string writeN = Console.ReadLine();
+// int N = Convert.ToInt32(writeN);
+
+
+// int[] GetCubeTable(int N)
+// {
+//     int[] cubs = new int[N + 1];
+//     for (int i = 1; i < cubs.Length; i++)
+//     {
+//         cubs[i] = i * i * i;
+//     }
+//     return cubs;
+// }
+
+// void PrintCubs(int[] array)
+// {
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         System.Console.Write($"{i} ^ 3 = ");
+//         System.Console.WriteLine(array[i]);
+//     }
+// }
+
+// int[] CubeTable = GetCubeTable(N);
+// PrintCubs(CubeTable);
+
