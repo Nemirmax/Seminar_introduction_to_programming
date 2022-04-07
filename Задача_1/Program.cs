67b032f99c091fa0b1cc6472ee3502a2bdaed699
Maxim <maxim.nemer@mail.ru>
2022-04-07 17:12:29 +0300

    добавили задачу 25

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -176,3 +176,26 @@
 // if (IsPalindrome(txtToCheck)) result = "является";
 // Console.WriteLine($"Введенное значение {result} палиндромом");
 // System.Console.WriteLine();
+// 25. Напишите цикл, который принимает на вход два числа (А и Б) и возводит
+// число А в натуральную степень В
+Console.Clear();
+System.Console.WriteLine();
+
+int number = 2;
+int exponent = 10;
+
+int GetExponentiation(int number, int exponent)
+{
+    int count = 1;
+    int result = number;
+    while (count < exponent)
+    {
+        result = result * number;
+        count++;
+    }
+    return result;
+}
+
+int result = GetExponentiation(number, exponent);
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
