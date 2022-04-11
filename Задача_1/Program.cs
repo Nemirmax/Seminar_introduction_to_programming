372a95549d7f604ba527638edec57ee479573725
Maxim <maxim.nemer@mail.ru>
2022-04-11 13:49:09 +0300

    добавили задачу 41

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -287,3 +287,21 @@
 // }
 
 // PrintArrayAndSum(FillArray(size));
+
+// Задача 41. Посчитать, сколько чисел больше 0 введено с клавиатуры
+
+Console.Clear();
+System.Console.WriteLine();
+int volume = 10;                // колчество ввода чисел
+int count = 0;
+
+while (volume > 0)
+{
+    System.Console.Write("Введите число:   ");
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if (number > 0) count++;
+    volume--;
+}
+
+System.Console.WriteLine($"Количество чисел больше 0: {count}");
