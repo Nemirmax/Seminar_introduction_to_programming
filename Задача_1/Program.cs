3eb31d2fbb99b5cbbc7d2bd5431996f2e9a13c9c
Maxim <maxim.nemer@mail.ru>
2022-04-11 13:18:54 +0300

    добавили задачу 36

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -224,34 +224,66 @@
 
 // Задача 34. Задать массив, заполнить случайными положительными трёхзначными числами. 
 // Показать количество нечетных\четных чисел
-Console.Clear();
-System.Console.WriteLine();
+// Console.Clear();
+// System.Console.WriteLine();
 
-int size = 20;
+// int size = 20;
 
-int[] FillArray(int size)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(100, 1000);
-    }
-    return array;
-}
+// int[] FillArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+//     return array;
+// }
 
-void PrintResult(int[] array)
-{
-    int even = 0;
-    int uneven = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write(array[i] + " ");
-        if (array[i] % 2 == 0) even++;
-        else uneven++;
-    }
-    System.Console.WriteLine();
-    System.Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {uneven}");
-    System.Console.WriteLine();
-}
+// void PrintResult(int[] array)
+// {
+//     int even = 0;
+//     int uneven = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//         if (array[i] % 2 == 0) even++;
+//         else uneven++;
+//     }
+//     System.Console.WriteLine();
+//     System.Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {uneven}");
+//     System.Console.WriteLine();
+// }
 
-PrintResult(FillArray(size));
+// PrintResult(FillArray(size));
+
+//  Задача 36. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+// Console.Clear();
+
+// int size = 20;
+
+// int[] FillArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(1, 100);
+//     }
+//     return array;
+// }
+
+// void PrintArrayAndSum(int[] arr)
+// {
+//     int sum = 0;
+//     System.Console.WriteLine();
+//     for (int i = 0; i < size; i++)
+//     {
+//         System.Console.Write(arr[i] + " ");
+//         if (i % 2 > 0) sum += arr[i];
+//     }
+//     System.Console.WriteLine();
+//     System.Console.WriteLine($"Сумма чисел, стоящих на нечетной позиции, соcтаваляет {sum} ");
+//     System.Console.WriteLine();
+// }
+
+// PrintArrayAndSum(FillArray(size));
